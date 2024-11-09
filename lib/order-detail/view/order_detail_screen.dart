487598c866042bb7bdc2dbbc14ed62c6318b33a6
yasir-bangash <yasirkhann824@gmail.com>
2024-11-09 13:40:23 +0500

    fixed ui issues

--- lib/order-detail/view/order_detail_screen.dart
+++ lib/order-detail/view/order_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nb_utils/nb_utils.dart';
-import 'package:zoovie/component/app_component.dart';
+// import 'package:zoovie/component/app_component.dart';
 import 'package:zoovie/component/custom_app_bar.dart';
 import 'package:zoovie/event-card-screen/component/event_card_component.dart';
 import 'package:zoovie/order-detail/component/order_detail_comp.dart';
@@ -16,20 +16,20 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffF2F4F6),
+      backgroundColor: const Color(0xffF2F4F6),
       body: Column(
         children: [
-          CustomAppBar(
+          const CustomAppBar(
             title: 'Order Detail',
             isCenter: true,
           ),
           Padding(
-            padding: EdgeInsets.symmetric(
+            padding: const EdgeInsets.symmetric(
               horizontal: 20,
             ),
             child: Column(
               children: [
-                EventCard(
+                const EventCard(
                   title: 'title',
                   date: '21-14-24',
                   location: 'NewYork',
@@ -37,7 +37,7 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
                       'https://images.unsplash.com/photo-1730637355825-3ec4372eb044?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 ),
                 10.height,
-                EventCard(
+                const EventCard(
                   title: 'title',
                   date: '21-14-24',
                   location: 'NewYork',
@@ -45,7 +45,7 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
                       'https://images.unsplash.com/photo-1730637355825-3ec4372eb044?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 ),
                 20.height,
-                PaymentComponent()
+                const PaymentComponent()
               ],
             ),
           ),
